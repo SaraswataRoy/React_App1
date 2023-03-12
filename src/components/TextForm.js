a82498632394ac399a9ca844f4cdf38c57e86f7f
Saraswata Roy <saraswataroy21@gmail.com>
2023-03-12 21:35:47 +0530

    Added more functions: World count, minutes read

---
+++ src/components/TextForm.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props){
+
+    const handleUpClick = () =>{
+        let newText = text.toUpperCase()
+        setText(newText)
+    }
+    const handleOnChange = (e) =>{
+        console.log("on Change")
+        setText(e.target.value)
+    }
+    const [text, setText] = useState('Enter Text Here')
+    return(
+        <>
+        <div className='container'>
+            <h1>{props.heading}</h1>
+            <div className='mb-3'>
+                <textarea className='form-control' value={text} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
+        </div>
+        <div className='container my-3'>
+            <h1>Your Text Summary</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008*text.split(" ").length} minutes read</p>
+        </div>
+        </>
+    )
+}
