5f369136d6e5d8de208c0868d973e0319ef3c41b
Saraswata Roy <saraswataroy21@gmail.com>
2023-03-19 15:33:52 +0530

    Added dark mode toggler

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+
 function App() {
+  const[Mode,  setMode] = useState('light')
+  const[alert, setAlert] = useState(null)
+  const showAlert = (message, type) =>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
+  const toggleMode = () =>{
+    if(Mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = 'black'
+      showAlert('Dark mode has been enabled', 'success')
+    }
+    else if(Mode === 'dark'){
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      showAlert('Dark mode has been disabled', 'success')
+    }
+  }
   return (
     <>
-      <Navbar/>
+      <Navbar Mode = {Mode} toggleMode = {toggleMode}/>
+      <Alert alert = {alert}/>
       <div className='container'>
-        <TextForm heading="Enter the text to analyze"/>
+        <TextForm heading="Enter text to analyze" Mode = {Mode} showAlert = {showAlert}/>
         {/* <About/> */}
       </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,22 +5,22 @@ export default function TextForm(props){
     const handleUpClick = () =>{
         let newText = text.toUpperCase()
         setText(newText)
+        props.showAlert("Converted to Upper case","success")
     }
     const handleOnChange = (e) =>{
-        console.log("on Change")
         setText(e.target.value)
     }
     const [text, setText] = useState('Enter Text Here')
     return(
         <>
-        <div className='container'>
+        <div className='container'style = {{color: props.Mode === 'dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className='mb-3'>
-                <textarea className='form-control' value={text} onChange = {handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className='form-control' value={text} onChange = {handleOnChange} style = {{backgroundColor: props.Mode === 'dark'?'grey':'white'}}id="myBox" rows="8"></textarea>
             </div>
             <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
         </div>
-        <div className='container my-3'>
+        <div className='container my-3'style = {{color: props.Mode === 'dark'?'white':'black'}}>
             <h1>Your Text Summary</h1>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008*text.split(" ").length} minutes read</p>
